894124868b154200fa08cd51ccc23e362e28f195
panhhuu <panhhuu@gmail.com>
2021-11-21 15:00:59 +0700

    Add UI: List class

--- src/containers/Classes/Classes.tsx
+++ src/containers/Classes/Classes.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
 import { batch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { MyClass } from '../../components/MyClass/MyClass';
 import { doGetListClasses, useAppDispatch, useAppSelector } from '../../redux';
 import './Classes.scss';
 
 export const Classes = () => {
   const dispatch = useAppDispatch();
-  const classes = useAppSelector((state) => state.classesSlice.classes);
+  const myClasses = useAppSelector((state) => state.classesSlice.classes);
 
   useEffect(() => {
     const initFetch = () => {
@@ -19,16 +20,29 @@ export const Classes = () => {
       });
     };
     initFetch();
-  });
+  }, []);
 
   return (
     <div className="classes">
-      {classes.map((c) => {
-        return <div>{c.name}</div>;
-      })}
-      <Link to="/" style={{ padding: 5 }}>
-        Home
-      </Link>
+      <ol className="joined">
+        {myClasses.map((item) => {
+          const randAvtMin = 1;
+          const randAvtMax = 7;
+          const randAvatar = `./assets/avatars/${Math.floor(
+            Math.random() * (randAvtMax - randAvtMin) + randAvtMin,
+          )}.png`;
+
+          return (
+            <MyClass
+              key={item._id}
+              _id={item._id}
+              name={item.name}
+              ownerName={'Phạm Minh Anh Hữu'}
+              ownerAvt={randAvatar}
+            />
+          );
+        })}
+      </ol>
     </div>
   );
 };
